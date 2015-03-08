952ab206784cb71db552b35e43303ac9ccbeedda
Richard Killam <richard.killam@gmail.com>
2015-03-07 20:56:45 -0400

    Added gitignore removed useless files

--- Microprocessor_ReferenceSystem/MainMemory_inst.vhd
+++
@@ -1,9 +0,0 @@
-MainMemory_inst : MainMemory PORT MAP (
-		address	 => address_sig,
-		clken	 => clken_sig,
-		clock	 => clock_sig,
-		data	 => data_sig,
-		rden	 => rden_sig,
-		wren	 => wren_sig,
-		q	 => q_sig
-	);
